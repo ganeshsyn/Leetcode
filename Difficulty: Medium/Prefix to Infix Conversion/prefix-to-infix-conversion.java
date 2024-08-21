245fe75338837566b8a0fe64aef32c597f2d8ec5
Ganesh Jana <156920200+ganeshsyn@users.noreply.github.com>
2024-08-21 09:12:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            String ans = obj.preToInfix(s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static String preToInfix(String pre_exp) {
+        Stack <String> stack=new Stack<>();
+        int i=pre_exp.length()-1;
+        while(i>=0){
+            String s = Character.toString(pre_exp.charAt(i));
+            if (s.length() == 1 && Character.isLetterOrDigit(s.charAt(0))) {
+                stack.push(s);
+            }
+            else{
+                String t1=stack.pop();
+                String t2=stack.pop();
+                String con="("+t1+s.charAt(0)+t2+")";
+                stack.push(con);
+            }
+            i--;
+        }
+        return stack.peek();
+    }
+}
