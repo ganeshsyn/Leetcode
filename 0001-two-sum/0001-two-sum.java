25f1780267812c29947f401e847a81821efcb906
Ganesh Jana <156920200+ganeshsyn@users.noreply.github.com>
2024-08-27 06:19:45 +0530

    Time: 4 ms (63.72%), Space: 44.5 MB (88.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,40 @@
+import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
+        // Better Solution
+        // List<Integer> al=Arrays.asList(nums);
+        // ArrayList<Integer> list=new ArrayList<>(list);
+        // Collections.addAll(list,nums);
+        // Collections.sort(list);
+        // int i=0,j=list.size()-1;
+        // while(i<j){
+        //     if(list.get(i)+list.get(j)<target){
+        //         i++;
+        //     }
+        //     else if(list.get(i)+list.get(j)>target){
+        //         j--;
+        //     }
+        //     else{
+        //         return new int[]{i,j};
+        //     }
+        // }
+        // return new int[] {};
+
+        // Better soln - using Hashmap
+        Map<Integer,Integer> numMap=new HashMap<>();
+        int n=nums.length;
+
+        //Build the hash table 
+        for(int i=0;i<n;i++){
+            numMap.put(nums[i],i);
+        }
+
+        // find complement
+        for(int i=0;i<n;i++){
+            int comp=target-nums[i];
+            // looking for a number in the array that, when added to the current number, equals the target, and ensuring that it’s not the same number being used twice.
+            if(numMap.containsKey(comp) && numMap.get(comp)!=i){
+                return new int[] {i,numMap.get(comp)};
             }
         }
         return new int[] {};
