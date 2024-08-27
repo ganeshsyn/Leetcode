cd360d5a74c8f87666d7bcb31de33e33b5c6d10e
Ganesh Jana <156920200+ganeshsyn@users.noreply.github.com>
2024-08-27 08:03:54 +0530

    Time: 0 ms (100%), Space: 42.1 MB (8.44%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,49 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // betetr solution
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                count0++;
+        // optimal soln - (O(N))
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
             }
-            else if(nums[i]==1){
-                count1++;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                count2++;
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                
+                high--;
             }
         }
-        for(int i=0;i<count0;i++){
-            nums[i]=0;
-        }
-        for(int i=0;i<count1;i++){
-            nums[i+count0]=1;
-        }
-        for(int i=0;i<count2;i++){
-            nums[i+count0+count1]=2;
-        }
+
+
+        // betetr solution (O(2N))
+        // int count0=0,count1=0,count2=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         count0++;
+        //     }
+        //     else if(nums[i]==1){
+        //         count1++;
+        //     }
+        //     else{
+        //         count2++;
+        //     }
+        // }
+        // for(int i=0;i<count0;i++){
+        //     nums[i]=0;
+        // }
+        // for(int i=0;i<count1;i++){
+        //     nums[i+count0]=1;
+        // }
+        // for(int i=0;i<count2;i++){
+        //     nums[i+count0+count1]=2;
+        // }
     }
 }
