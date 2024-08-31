45597034b58891efdc9aabb56685f7a7560e049f
Ganesh Jana <156920200+ganeshsyn@users.noreply.github.com>
2024-08-31 09:07:44 +0530

    Time: 0 ms (100%), Space: 42.5 MB (5.11%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,19 +1,40 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int [][] newMatrix=new int[n][n];
-        // copy all the eleemnts to a new matrix
+        // optimal soln
+        // Transpose 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                newMatrix[i][j]=matrix[i][j];
+            for(int j=i;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        // then change it
+        // Reverse a col
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[j][n-1-i]=newMatrix[i][j];
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
             }
         }
+
+
+
+        // Brute force soln
+        // int [][] newMatrix=new int[n][n];
+        // // copy all the eleemnts to a new matrix
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         newMatrix[i][j]=matrix[i][j];
+        //     }
+        // }
+        // // thenchange 
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         matrix[j][n-1-i]=newMatrix[i][j];
+        //     }
+        // }
 
     }
 }
